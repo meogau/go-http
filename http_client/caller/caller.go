ee5674317b1ea2abfd5d96628bd0843d06352841
meogau <thainguyen1421999@gmail.com>
2024-05-06 14:22:46 +0700

    chore: implement caller SendRequest method

--- http_client/caller/caller.go
+++ http_client/caller/caller.go
@@ -1,9 +1,11 @@
 package caller
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -33,6 +35,50 @@ func (caller *Caller) Get(ctx context.Context, url string, queryMap map[string]s
 	if err != nil {
 		return nil, err
 	}
+	defer func() {
+		err := res.Body.Close()
+		if err != nil {
+			fmt.Printf("Error close body response: %v", err)
+		}
+	}()
+	var apiResponse map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&apiResponse)
+	if err != nil {
+		return nil, err
+	}
+	return apiResponse, nil
+}
+
+func (caller *Caller) SendRequest(ctx context.Context, method string, url string, queryMap map[string]string, headerMap map[string]string,
+	bodyRequest interface{}) (map[string]interface{}, error) {
+
+	maxConnWaitTime := caller.Client.Timeout
+	ctx, cancel := context.WithTimeout(ctx, maxConnWaitTime)
+	defer cancel()
+
+	jsonData, err := json.Marshal(bodyRequest)
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Printf("error when create new request: %v", err)
+		return nil, err
+	}
+
+	for key, value := range headerMap {
+		req.Header.Set(key, value)
+	}
+
+	query := req.URL.Query()
+	for key, val := range queryMap {
+		query.Add(key, val)
+	}
+	req.URL.RawQuery = query.Encode()
+
+	res, err := caller.Client.Do(req)
+	if err != nil {
+		log.Printf("Error sending request to API endpoint. %+v", err)
+		return nil, err
+	}
+
 	defer func() {
 		err := res.Body.Close()
 		if err != nil {
--- http_client/main/main.go
+++ http_client/main/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"go-http/http_client/caller"
 	"go-http/http_client/high_performance_client"
+	"net/http"
 	"time"
 )
 
@@ -13,12 +14,23 @@ func main() {
 	httpCaller := caller.Caller{
 		Client: client,
 	}
+
+	//slow in first request
+	start := time.Now()
+	mapResponse, _ := httpCaller.Get(context.Background(), "https://httpbin.org/get", nil)
+	fmt.Printf("First time request - takes time: %v ms\n", time.Since(start))
+	for key, val := range mapResponse {
+		fmt.Printf(" %v : %v\n", key, val)
+	}
+	//faster in the latter request cuz reuse connection
 	for i := 0; i < 10; i++ {
 		start := time.Now()
-		mapResponse, _ := httpCaller.Get(context.Background(), "https://httpbin.org/get", nil)
-		for key, val := range mapResponse {
-			fmt.Printf(" %v : %v\n", key, val)
-		}
-		fmt.Printf("Request %v - take time: %v ms\n", i, time.Since(start))
+		mapResponse, _ = httpCaller.Get(context.Background(), "https://httpbin.org/get", nil)
+		fmt.Printf("Request %v - takes time: %v ms\n", i+2, time.Since(start))
+	}
+	//test method post
+	mapResponse, _ = httpCaller.SendRequest(context.Background(), http.MethodPost, "https://httpbin.org/post", nil, nil, map[string]string{"foo": "baz"})
+	for key, val := range mapResponse {
+		fmt.Printf(" %v : %v\n", key, val)
 	}
 }
